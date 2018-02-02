6aeb0178b4d9b4ecbc96e9537b5ae9da72a5aef4
jaroslaw <jarek.skrzypczak@o2.pl>
2018-02-02 17:53:02 +0100

    Refactor

--- app/src/main/java/com/example/jaroslaw/sw_project/TrainingTrackActivity.kt
+++ app/src/main/java/com/example/jaroslaw/sw_project/TrainingTrackActivity.kt
@@ -1,37 +1,34 @@
 package com.example.jaroslaw.sw_project
 
 import android.database.sqlite.SQLiteDatabase
-import android.location.Location
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
 import android.util.Log
 import android.view.View
 import android.widget.AdapterView
 import android.widget.ArrayAdapter
-import com.google.android.gms.maps.CameraUpdate
 import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.PolylineOptions
 import kotlinx.android.synthetic.main.activity_training_track.*
 import java.util.*
-import com.google.android.gms.maps.GoogleMap
-import com.google.android.gms.maps.model.LatLng
-import com.google.android.gms.maps.model.MarkerOptions
-import com.google.android.gms.maps.model.PolylineOptions
 
 
 class TrainingTrackActivity : AppCompatActivity(), OnMapReadyCallback {
 
     private var mMap: GoogleMap? = null
     private val TAG = "TrainingTrackActivity"
-    private var latLngList : LinkedList<LatLng>? = LinkedList()
-    private var bundle : Bundle? = null
-    private val CENTER_POLAND : LatLng = LatLng(51.9194,19.1451)
+    private var latLngList: LinkedList<LatLng>? = LinkedList()
+    private var bundle: Bundle? = null
+    private val CENTER_POLAND: LatLng = LatLng(51.9194, 19.1451)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_training_track)
         bundle = savedInstanceState
-        var list : LinkedList<String> = LinkedList()
+        var list: LinkedList<String> = LinkedList()
         synchronized(this) {
             val databaseManager = DatabaseManager(this)
             val readDatabase: SQLiteDatabase = databaseManager!!.readableDatabase
@@ -43,7 +40,7 @@ class TrainingTrackActivity : AppCompatActivity(), OnMapReadyCallback {
         val arrayAdapter = ArrayAdapter(this, android.R.layout.simple_spinner_dropdown_item, list.toArray())
         training_chooser_spinner.adapter = arrayAdapter
 
-        training_chooser_spinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener{
+        training_chooser_spinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
             override fun onNothingSelected(p0: AdapterView<*>?) {
             }
 
@@ -51,7 +48,7 @@ class TrainingTrackActivity : AppCompatActivity(), OnMapReadyCallback {
                 try {
                     val selectedItem = parent!!.getItemAtPosition(position).toString()
                     getTrainingFromDatabase(selectedItem.toLong())
-                }catch (e : NumberFormatException){
+                } catch (e: NumberFormatException) {
                     latLngList = LinkedList()
                     mapView.onDestroy()
                     mapView.onCreate(bundle)
@@ -66,16 +63,15 @@ class TrainingTrackActivity : AppCompatActivity(), OnMapReadyCallback {
         mapView.getMapAsync(this)
     }
 
-    private fun getTrainingFromDatabase(trainingID : Long){
-        synchronized(this){
+    private fun getTrainingFromDatabase(trainingID: Long) {
+        synchronized(this) {
             val databaseManager = DatabaseManager(this)
-            val readDatabase : SQLiteDatabase = databaseManager!!.readableDatabase
+            val readDatabase: SQLiteDatabase = databaseManager!!.readableDatabase
             val training = databaseManager!!.selectTraining(readDatabase, trainingID)
             latLngList = LinkedList()
-            for (measurement : Measurement in training.getTrainingHistory()){
-                val location : Location = measurement.getLocation()
-                latLngList!!.addLast(LatLng(location.latitude,location.longitude))
-            }
+            training.getTrainingHistory()
+                    .map { it.getLocation() }
+                    .forEach { latLngList!!.addLast(LatLng(it.latitude, it.longitude)) }
             Log.d(TAG, "latLngList : $latLngList")
             mapView.onDestroy()
             mapView.onCreate(bundle)
@@ -87,10 +83,10 @@ class TrainingTrackActivity : AppCompatActivity(), OnMapReadyCallback {
     override fun onMapReady(googleMap: GoogleMap?) {
         mMap = googleMap
         mMap!!.addPolyline(PolylineOptions().addAll(latLngList))
-        if (latLngList!!.isEmpty()){
-            mMap!!.moveCamera(CameraUpdateFactory.newLatLngZoom(CENTER_POLAND,5.5f))
-        }else{
-            mMap!!.moveCamera(CameraUpdateFactory.newLatLngZoom(latLngList!!.first,10f))
+        if (latLngList!!.isEmpty()) {
+            mMap!!.moveCamera(CameraUpdateFactory.newLatLngZoom(CENTER_POLAND, 5.5f))
+        } else {
+            mMap!!.moveCamera(CameraUpdateFactory.newLatLngZoom(latLngList!!.first, 10f))
         }
     }
 }
